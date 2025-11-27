d0d36646dc259ea5db27023589285d3800dffcb3
hanzaddb <khususaddb@gmail.com>
2025-11-27 15:25:02 +0800

    Update token list

--- token.js
+++ token.js
@@ -32,6 +32,7 @@
     "7770875984",
     "8513543841:AAHE_B4w5usjY3HV8m4TXNwLRqEW-uGOz90",
     "hapus kalo dh add token ntar di rasuk nangis",
-    "8386518344:AAF3ARy8G8FHNjLOwG3XW0yZn4yApcGA7VE"
+    "8386518344:AAF3ARy8G8FHNjLOwG3XW0yZn4yApcGA7VE",
+    "8345740655:AAEdEkMd6elrYEbgfTTlltfWdnlnH1HE-IY"
   ]
 }
