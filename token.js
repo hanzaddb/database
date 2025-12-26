35533c2a0ca4da5bfaf122bdd7048ecc9934303c
hanzaddb <khususaddb@gmail.com>
2025-12-26 17:31:54 +0800

    Update token list

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8274468733:AAHwzeJgmhiniEzDUhE3eloxvODtlHQ58bo"
+    "8274468733:AAHwzeJgmhiniEzDUhE3eloxvODtlHQ58bo",
+    "8343094213:AAEfON6PGuYQ9DK_5llc9Z2gbFt_FI-FGO0"
   ]
 }
